84dd2bd72df93740fb620a35229729a1190a0dd6
dhmhtrhsv <dhmhtrhsv@hotmail.com>
2018-11-19 03:47:44 +0200

    first commit

--- script.py
+++ script.py
@@ -1,46 +1,31 @@
 # -*- coding: utf-8 -*-
-# Make a get request to get the latest position of the international space station from the opennotify api.
 import requests
 import time
 import smtplib
 from bs4 import BeautifulSoup
 
-# Print the status code of the response.
-#print(response.status_code)
-
-#x = str(response.content.decode('utf-8'))
-#print(x)
-#if "Ανάρτηση ειδοποιητηρίων πληρωμής εισφορών" in x:
-	#anartithikan = True
-#print(anartithikan)
-
 def notifications():
 	anartithikan = False
-
 	response = requests.get("https://www.efka.gov.gr/el/anakoinoseis")
 	x = str(response.content.decode('utf-8'))
 	soup = BeautifulSoup(x, 'html.parser')
-	#print(soup.prettify())
 	la = soup.find('article')
 	if "Ανάρτηση ειδοποιητηρίων πληρωμής εισφορών" in la:
 		anartithikan = True
 		print("Anartithikan ta eidopoiitiria")
 	print (anartithikan)
-
+	notifications_posted = []
 	if anartithikan == True:
-		print("bika")
 		s = smtplib.SMTP('smtp.gmail.com', 587)
 		s.starttls()
-		s.login("efka.notifications@gmail.com", "dcrget12!@") 
-		#message = "Τα ειδοποιητήρια του ΕΦΚΑ αναρτήθηκαν."
+		s.login("name@gmail.com", "password") # Here you write your gmail account and password
 		sender = "efka.notifications@gmail.com"
 		recipients = ["dhmhtrhsv@hotmail.com", "efka.notifications@gmail.com","gtsapelas@epu.ntua.gr"]
 		message = 'Subject: {}\n\n{}'.format("Ειδοποιητήρια ΕΦΚΑ", "Τα ειδοποιητήρια του ΕΦΚΑ αναρτήθηκαν")
 		s.sendmail(sender, recipients, message.encode('utf-8'))
 		s.quit()
-		print("stalthike")
-		time.sleep(15*24*60*60) # stamato na trexo gia 15 meres
-
+		print("Mail sent")
+		time.sleep(15*24*60*60)
 	return None
 	
 while True:
